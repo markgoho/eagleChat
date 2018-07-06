85f74f98e21f29b358caf692b3f676737cf9991d
Mark Goho <markgoho@gmail.com>
2018-07-05 22:00:58 -0400

    uncomment firebase sw

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,10 +1,10 @@
-// importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.1.0/firebase-messaging.js');
 
-// // Initialize the Firebase app in the service worker by passing in the
-// // messagingSenderId.
-// firebase.initializeApp({
-//   // TODO add your messagingSenderId
-//   messagingSenderId: '824605516628',
-// });
-// var messaging = firebase.messaging();
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  // TODO add your messagingSenderId
+  messagingSenderId: '824605516628',
+});
+var messaging = firebase.messaging();
