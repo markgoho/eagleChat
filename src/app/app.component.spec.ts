b2f9dea6388fd39089ac2da9857302183db7da30
Mark Goho <markgoho@gmail.com>
2018-07-05 21:48:44 -0400

    Updates to get things working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -26,6 +22,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to eagleChat!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to eagleChat!'
+    );
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MatSnackBarModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { StylizePipe } from './stylize.pipe';
+
+const configErrMsg = `You have not configured and imported the Firebase SDK.
+Make sure you go through the codelab setup instructions.`;
+
+const bucketErrMsg = `Your Firebase Storage bucket has not been enabled. Sorry
+about that. This is actually a Firebase bug that occurs rarely. Please go and
+re-generate the Firebase initialization snippet (step 4 of the codelab) and make
+sure the storageBucket attribute is not empty. You may also need to visit the
+Storage tab and paste the name of your bucket which is displayed there.`;
+
+if (!environment.firebase) {
+  if (!environment.firebase.apiKey) {
+    window.alert(configErrMsg);
+  } else if (environment.firebase.storageBucket === '') {
+    window.alert(bucketErrMsg);
+  }
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, StylizePipe],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence(),
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
