f3c50b3e0d690eb1cf0c5de9dac57d1e41db684f
Mark Goho <markgoho@gmail.com>
2018-07-05 20:54:49 -0400

    working circle config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDKY7tAYA9jwH9oJpzmT9nOBxsXahavZN8',
+    authDomain: 'eagle-chat-b39d7.firebaseapp.com',
+    databaseURL: 'https://eagle-chat-b39d7.firebaseio.com',
+    projectId: 'eagle-chat-b39d7',
+    storageBucket: 'eagle-chat-b39d7.appspot.com',
+    messagingSenderId: '824605516628',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDKY7tAYA9jwH9oJpzmT9nOBxsXahavZN8',
+    authDomain: 'eagle-chat-b39d7.firebaseapp.com',
+    databaseURL: 'https://eagle-chat-b39d7.firebaseio.com',
+    projectId: 'eagle-chat-b39d7',
+    storageBucket: 'eagle-chat-b39d7.appspot.com',
+    messagingSenderId: '824605516628',
+  },
 };
 
 /*
